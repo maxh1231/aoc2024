c9306df42ca3d7ac77b258fd6e62e0aa8eed5f64
MaxHumpherys <maxhumpherys@gmail.com>
2024-12-08 19:18:15 -0700

    day3 part 2

--- day3/index.js
+++ day3/index.js
@@ -2,12 +2,27 @@ const fs = require('fs');
 let data = fs.readFileSync('input.txt', 'utf-8');
 let arr = (data.match(/mul\(\d+,\d+\)/g))
 let part1 = 0;
+let part2 = 0;
 
 arr.forEach((item) => {
-    let valid = item.match(/\d+/g).map(Number)
+    let isSafe = true;
+    let valid = item.match(/\d+/g).map(Number);
     let product = valid.reduce((a, b) => a * b);
+    let substr = data.substring(0, data.indexOf(item));
+    let rev = substr.split('').reverse().join('');
+    let redLight = rev.search(/\)\(t'nod/g);
+    let greenLight = rev.search(/\)\(od/g);
+
+    if (redLight != -1 && greenLight == -1) {
+        isSafe = false;
+    }
+
+    if (greenLight > redLight && greenLight != -1) isSafe = false;
 
     part1 += product;
-    console.log(part1);
 
+    if (isSafe) part2 += product;
 })
+
+console.log("Part 1: " + part1);
+console.log("Part 2: " + part2);
